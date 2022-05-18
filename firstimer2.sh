34fc2773b7e74dfa1ccbeaff6f5e2f5ee62d59eb
ilanl <ilan.lavan@kenshoo.com>
2022-05-18 11:20:24 +0300

    wip

--- firstimer2.sh
+++ firstimer2.sh
@@ -9,21 +9,83 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Check for Homebrew,
-# Install if we don't have it
-if test ! $(which brew); then
-  echo "Installing homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
+## Install if we don't have it
+#if test ! $(which brew); then
+#  echo "Installing homebrew..."
+#  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#fi
 
-#adding Homebrew to your PATH:
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$USER/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
+##adding Homebrew to your PATH:
+#echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$USER/.zprofile
+#eval "$(/opt/homebrew/bin/brew shellenv)"
 
-# Make sure we’re using the latest Homebrew.
-brew update
+## Make sure we’re using the latest Homebrew.
+#brew update
 
-# Upgrade any already-installed formulae.
-brew upgrade --formula
+## Upgrade any already-installed formulae.
+#brew upgrade --formula
+
+## install zsh
+echo "Installing zsh..."
+brew install zsh
+echo 'eval "parse_git_branch() { git branch 2> /dev/null | sed -n -e 's/^\* \(.*\)/[\1]/p' } COLOR_DEF=$'\e[0m' COLOR_USR=$'\e[38;5;243m' COLOR_DIR=$'\e[38;5;197m' COLOR_GIT=$'\e[38;5;39m' NEWLINE=$'\n' setopt PROMPT_SUBST export PROMPT='${COLOR_USR}%n@%M ${COLOR_DIR}%d ${COLOR_GIT}$(parse_git_branch)${COLOR_DEF}${NEWLINE}%% '"' >> /Users/$USER/.zshrc
+source ~/.zshrc
+
+#install java
+echo "Installing java..."
+echo export "JAVA_HOME=\$(/usr/libexec/java_home)" >> ~/.bash_profile
+brew install java
+
+# install tomcat
+echo "Installing tomcat..."
+brew install tomcat@9
+
+# install docker
+echo "Installing docker..."
+brew install --cask docker
+
+## Jfrog logindocker - need to get credentials from init.gradle
+#docker login -u <USER> -p <PASSWORD> kenshoo-docker.jfrog.io
+
+# ecr login
+# Create the following file: ~/.aws/credentials if it doesn’t exist.
+#aws_access_key_id=
+#aws_secret_access_key=
+#aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 668139184987.dkr.ecr.us-east-1.amazonaws.com
+
+# install gradle
+echo "Installing gradle..."
+brew install gradle
+#get init.gradle and place it in ~/.gradle
+
+
+# install git
+echo "Installing git..."
+brew install git
+
+#add ssh key 
+#https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
+
+#Configure vault
+echo "Installing hashicorp..."
+brew install hashicorp/tap/vault
+cd ~
+vault login -address="https://vault-staging.internalk-stg.com:8200" -method=okta username=<your username>
+
+#Install volta
+echo "Installing volta..."
+brew install volta
+export PATH=$VOLTA_HOME/bin:$PATH
+
+#Clone search
+echo "Clone search..."
+mkdir ~/dev
+cd ~/dev
+git clone git@github.com:kenshoo/search.git
+
+
+
+
 
 # Install GNU core utilities (those that come with OS X are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
@@ -170,6 +232,7 @@ brew install boot2docker
 
 # Install developer friendly quick look plugins; see https://github.com/sindresorhus/quick-look-plugins
 brew install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzip qlimagesize webpquicklook suspicious-package quicklookase  --cask
+
 
 # Remove outdated versions from the cellar.
 brew cleanup
