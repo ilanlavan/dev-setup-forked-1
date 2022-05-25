a7133dd7da50cdadf6906cf4186c7dec36fcb5da
ilanl <ilan.lavan@kenshoo.com>
2022-05-25 12:01:08 +0300

    wip

--- firstimer3.sh
+++ firstimer3.sh
@@ -19,8 +19,8 @@ echo "Installing docker..."
   ln -sfn /opt/homebrew/opt/docker-compose/bin/docker-compose ~/.docker/cli-plugins/docker-compose
 
 echo "{\"composeV2\": \"disabled\"}" >> ~/.docker/features.json
-
-timezone = sudo systemsetup -gettimezone
+sudo rm ~/etc/localtime
+timezone=systemsetup -gettimezone
 sudo ln -sf /usr/share/zoneinfo/$timezone/etc/localtime
 echo $timezone >> ~/etc/timezone
 
