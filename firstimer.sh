1c0deb407c0bfe0d1cdd043d3c55a712b355127f
ilanl <ilan.lavan@kenshoo.com>
2022-05-30 17:45:37 +0300

    using script to get vault-token from vault

--- firstimer.sh
+++ firstimer.sh
@@ -11,7 +11,9 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 # Check for Homebrew,
 # Install if we don't have it
 if test ! $(which brew); then
+  echo "*********************************************************"
   echo "Installing homebrew..."
+  echo "*********************************************************"
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
@@ -19,157 +21,200 @@ fi
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/$USER/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-# Make sure we’re using the latest Homebrew.
+## Make sure we’re using the latest Homebrew.
 brew update
 
-# Upgrade any already-installed formulae.
+## Upgrade any already-installed formulae.
 brew upgrade --formula
 
-# Install GNU core utilities (those that come with OS X are outdated).
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
-sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+# install zsh
+echo "*********************************************************"
+echo "Installing zsh..."
+echo "*********************************************************"
+brew install zsh
+echo " parse_git_branch() { git branch 2> /dev/null | sed -n -e 's/^\* \(.*\)/[\1]/p' } COLOR_DEF=\$'\e[0m' COLOR_USR=\$'\e[38;5;243m' COLOR_DIR=\$'\e[38;5;197m' COLOR_GIT=\$'\e[38;5;39m' NEWLINE=\$'\n' setopt PROMPT_SUBST export PROMPT='\${COLOR_USR}%n@%M \${COLOR_DIR}%d \${COLOR_GIT}\$(parse_git_branch)\${COLOR_DEF}\${NEWLINE}%% '" >> ~/.zshrc
+source ~/.zshrc
 
-# Install some other useful utilities like `sponge`.
-brew install moreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed
-# Install Bash 4.
-brew install bash
-brew install bash-completion2
-# We installed the new shell, now we have to activate it
-echo "Adding the newly installed shell to the list of allowed shells"
-# Prompts for password
-sudo bash -c 'echo /usr/local/bin/bash >> /etc/shells'
-# Change to the new shell, prompts for password
-chsh -s /usr/local/bin/bash
+#install java
+echo "*********************************************************"
+echo "Installing java..."
+echo "*********************************************************"
+echo export "JAVA_HOME=\$(/usr/libexec/java_home)" >> ~/.bash_profile
+#fix java version to be 11
+brew --cask install java11
 
-# Install `wget`.
-brew install wget
+# install tomcat
+echo "*********************************************************"
+echo "Installing tomcat..."
+echo "*********************************************************"
+brew install tomcat@9
 
-# Install RingoJS and Narwhal.
-# Note that the order in which these are installed is important;
-# see http://git.io/brew-narwhal-ringo.
-brew install ringojs
-brew install narwhal
+#set timezone
+echo "*********************************************************"
+echo "Setting timezone"
+echo "*********************************************************"
 
-# Install Python
-brew install python
-brew install python3
+timezonemessage=$(sudo systemsetup -gettimezone)
+echo $timezonemessage
+prefix="Time Zone: "
+timezone=${timezonemessage#"$prefix"}
+echo sudo ln -sf /usr/share/zoneinfo/$timezone/etc/localtime
+sudo ln -sf /usr/share/zoneinfo/$timezone/etc/localtime
+mkdir ~/etc
+echo $timezone > ~/etc/timezone
 
-# Install ruby-build and rbenv
-brew install ruby-build
-brew install rbenv
-LINE='eval "$(rbenv init -)"'
-grep -q "$LINE" ~/.extra || echo "$LINE" >> ~/.extra
 
-# Install more recent versions of some OS X tools.
-brew install vim
-brew link vim
-brew install php@5.6
+## install docker -
+#todo : validate that m1 docker https://www.docker.com/products/docker-desktop/ is installed
+echo "*********************************************************"
+echo "Installing docker..."
+echo "*********************************************************"
+brew install --cask docker
 
-# Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
+##Compose is now a Docker plugin. For Docker to find this plugin, symlink it:
+  mkdir -p ~/.docker/cli-plugins
+  ln -sfn /opt/homebrew/opt/docker-compose/bin/docker compose ~/.docker/cli-plugins/docker-compose
 
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install homebrew/x11/xpdf
-brew install xz
+echo "*********************************************************"
+echo "disable compose v2 on docker"
+echo "*********************************************************"
+echo "{\"composeV2\": \"disabled\"}" >> ~/.docker/features.json
 
-# Install other useful binaries.
-brew install ack
-brew install dark-mode
-#brew install exiv2
+echo "*********************************************************"
+echo "set a local container for the docker file sharing"
+echo "*********************************************************"
+sed -i.bak 's#"dev/search/kenshoo/java/docker/tomcat",#"dev/search/kenshoo/java/docker/tomcat","/kenshoo/java/docker/tomcat/KS-Logs",#g' ~/Library/Group\ Containers/group.com.docker/settings.json
+
+open /Applications/Docker.app
+
+#todo : authenticate via vault - needs verification
+
+
+#Configure vault
+echo "*********************************************************"
+echo "Installing hashicorp..."
+echo "*********************************************************"
+brew install hashicorp/tap/vault
+brew install jq
+
+echo "*********************************************************"
+echo "setting vault-token file..."
+echo "*********************************************************"
+PS3='Please enter your choice: '
+options=("vault-prod" "vault-staging" "quit")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "vault-prod")
+            echo "you chose $opt"
+            VAULT_URL="https://vault.internalk.com:8200/v1/auth/okta/login"
+            break
+            ;;
+        "vault-staging")
+            echo "you chose $opt"
+            VAULT_URL="https://vault-staging.internalk-stg.com:8200/v1/auth/okta/login"
+            break
+            ;;
+        "quit")
+            exit
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+
+USERNAME=$(whoami)
+read -sp "please enter password: " PASSWORD
+TOKEN=$(curl -s --request POST --data "{\"password\": \"${PASSWORD}\"}" "${VAULT_URL}"/"${USERNAME}" | jq -r .auth.client_token)
+echo -e "\nYour token is: $TOKEN"
+echo $TOKEN > ~/.vault-token
+perl -p -i -e 's/\R//g;' ~/.vault-token
+
+
+## Jfrog logindocker - need to get credentials from init.gradle
+#docker login -u <USER> -p <PASSWORD> kenshoo-docker.jfrog.io
+
+# ecr login
+# Create the following file: ~/.aws/credentials if it doesn’t exist.
+#aws_access_key_id=
+#aws_secret_access_key=
+#aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 668139184987.dkr.ecr.us-east-1.amazonaws.com
+
+# install gradle
+echo "*********************************************************"
+echo "Installing gradle..."
+echo "*********************************************************"
+brew install gradle
+#get init.gradle and place it in ~/.gradle
+
+
+# install git
+echo "*********************************************************"
+echo "Installing git..."
+echo "*********************************************************"
 brew install git
-brew install git-lfs
-brew install git-flow
-brew install git-extras
-brew install hub
-#brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install rhino
-brew install speedtest_cli
-brew install ssh-copy-id
-brew install tree
-brew install webkit2png
-brew install zopfli
-brew install pkg-config libffi
-brew install pandoc
 
-# Lxml and Libxslt
-brew install libxml2
-brew install libxslt
-brew link libxml2 --force
-brew link libxslt --force
 
-# Install Heroku
-brew install heroku/brew/heroku
-heroku update
+#Install volta
+echo "*********************************************************"
+echo "Installing volta..."
+echo "*********************************************************"
+#brew install volta
+#export PATH=$VOLTA_HOME/bin:$PATH
 
-# Core casks
-brew install --appdir="/Applications" alfred --cask
-brew install --appdir="~/Applications" iterm2 --cask
-brew cask install --appdir="~/Applications" java
-brew install --appdir="~/Applications" xquartz --cask
 
-# Development tool casks
-brew install --appdir="/Applications" sublime-text --cask
-brew install --appdir="/Applications" atom --cask
-brew install --appdir="/Applications" virtualbox --cask
-brew install --appdir="/Applications" vagrant --cask
-brew install --appdir="/Applications" macdown --cask
+#add ssh key
+#https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
 
-# Misc casks
-brew install --appdir="/Applications" google-chrome --cask
-brew install --appdir="/Applications" firefox --cask
-brew install --appdir="/Applications" skype --cask
-brew install --appdir="/Applications" slack --cask
-brew install --appdir="/Applications" dropbox --cask
-brew install --appdir="/Applications" evernote --cask
-brew install --appdir="/Applications" 1password --cask
-#brew cask install --appdir="/Applications" gimp
-#brew cask install --appdir="/Applications" inkscape
+echo "Generating SSH key..."
+echo "*************************************************************************************************************"
+echo "*************************************************************************************************************"
+echo "** press enter for all questions...                                                                        **"
+echo "**                                                                                                         **"
 
-#Remove comment to install LaTeX distribution MacTeX
-#brew cask install --appdir="/Applications" mactex
+#check how to remove the key name from the next line
+#todo : pbcopy to copy to clipboard the content of the id_rsa
+ssh-keygen -t rsa -b 2048
+echo "*************************************************************************************************************"
+echo "**Goto your github account and paste the following code under Settings-> SSH and GPG keys -> New SSH key...**"
+echo "**                                   https://github.com/settings/keys                                      **"
+echo "**           Once you are done authorize the key via the Authorize button under the new generated key      **"
+echo "*************************************************************************************************************"
+echo "*************************************************************************************************************"
+cat ~/.ssh/id_rsa.pub
 
-# Install Docker, which requires virtualbox
-brew install docker
-brew install boot2docker
+read -p "Press enter to continue"
 
-# Install developer friendly quick look plugins; see https://github.com/sindresorhus/quick-look-plugins
-brew install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzip qlimagesize webpquicklook suspicious-package quicklookase  --cask
+#todo ask the user to print yes so he can use the key and add github to the list of authorized urls
+
+#Clone search
+echo "*********************************************************"
+echo "Clone search..."
+echo "*********************************************************"
+mkdir ~/dev
+cd ~/dev
+git clone git@github.com:kenshoo/search.git
+
+echo "*********************************************************"
+echo "Prepare KS env"
+echo "*********************************************************"
+#download keystore and rename
+#download server.xml, apply changes and locate here /opt/homebrew/Cellar
+
+cd ~dev/search/kenshoo/java
+./gradlew clean cleanIdea idea
+
+#Edit “performanceAggregation.properties” file:
+#file location - kenshoo/java/flat-view/flat-view-business-impl/src/main/resources/performanceAggregation.properties
+#change needed -
+#paggr.host=localhost
+#paggr.port=9090
+
+
+echo "*********************************************************"
+echo "Create local DB"
+echo "*********************************************************"
+#cd ~dev/search/kenshoo/java/db
+#./gradlew createNewDb genconf genDBProps genTestDBProps update liquidpackage -PdbPass=root56 -PdbUser=root -PdbName=kazaam -PdbHost=localhost -Pforce=true
 
 # Remove outdated versions from the cellar.
 brew cleanup
